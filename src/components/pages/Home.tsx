3a0d21378448971b22f37823403525951d93f925
cendekia <cendekia@sonar.id>
2021-11-06 17:21:29 +0700

    add Hero

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,4 +1,12 @@
-import { FiSearch } from "react-icons/fi";
+import { FiSearch } from "react-icons/fi"
+import styled from "styled-components"
+
+const Hero = styled.div`
+  background-image: url("/images/header.jpeg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`
 
 const Home = () => {
   return (
@@ -16,17 +24,42 @@ const Home = () => {
             />
           </div>
           <div className="w-1/6 flex justify-end">
-            <a href="/login" className="mx-3 text-gray-500 hover:text-gray-800" rel="noopener noreferrer">
+            <a
+              href="/login"
+              className="mx-3 text-gray-500 hover:text-gray-800"
+              rel="noopener noreferrer"
+            >
               Log In
             </a>
-            <a href="/signup" className="mx-3 text-gray-500 hover:text-gray-800" target="_blank" rel="noopener noreferrer">
+            <a
+              href="/signup"
+              className="mx-3 text-gray-500 hover:text-gray-800"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Sign Up
             </a>
           </div>
         </div>
       </header>
-      <div>
-        <div></div>
+      <div className="container max-w-6xl mx-auto flex p-5">
+        <Hero className="w-full h-80 my-6 rounded relative">
+          <div className="bg-black opacity-30 absolute w-full h-80"></div>
+          <div className="text-right m-8">
+            <a
+              href="/save-collection"
+              className="p-3 bg-black text-white text-sm rounded-full opacity-75"
+            >
+              + Save Collection
+            </a>
+          </div>
+          <div className="text-white absolute bottom-0 m-10">
+            <h1 className="text-5xl">Trending in Jakarta</h1>
+            <p>
+              Most-loved & highly-reviewed restaurants, by and for Jakartans!
+            </p>
+          </div>
+        </Hero>
       </div>
     </>
   )
